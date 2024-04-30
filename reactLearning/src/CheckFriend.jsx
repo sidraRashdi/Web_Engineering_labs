aa4401228354dea7b497f3a458bfcc0fa376b5c2
sidraRashdi <rashdimuntaha557@gmail.com>
2024-05-01 01:57:20 +0500

    done with achaDost buraDost

--- reactLearning/src/CheckFriend.jsx
+++ reactLearning/src/CheckFriend.jsx
@@ -9,6 +9,21 @@ function checkFriend(props) {
     acha: "Acha",
     bura: "Bura",
   };
+
+  const revertUser = (index, name, Frndcategory) => {
+    const indexOfUser = users.findIndex(
+      (item) => item.name === name && item.Category === Frndcategory
+    );
+    // console.log(category);
+    const updatedUsers = [...users];
+    // console.log(JSON.stringify(updatedUsers));
+    // console.log(updatedUsers[indexOfUser].Category);
+    updatedUsers[indexOfUser].Category =
+      Frndcategory === category.acha ? category.bura : category.acha;
+    // console.log(JSON.stringify(updatedUsers));
+    // (JSON.stringify(users[index]));
+    setUsers(updatedUsers);
+  };
   const TypeOfFriend = (e) => {
     setCategory(e.target.value);
   };
@@ -35,7 +50,10 @@ function checkFriend(props) {
         {users
           ?.filter((item) => item.Category === category.acha)
           ?.map((user, index) => (
-            <li key={index} onClick={() => alert(index)}>
+            <li
+              key={index}
+              onClick={() => revertUser(index, user.name, user.Category)}
+            >
               {user.name} ({user.Category})
             </li>
           ))}
@@ -46,7 +64,10 @@ function checkFriend(props) {
         {users
           ?.filter((item) => item.Category === category.bura)
           ?.map((user, index) => (
-            <li key={index} onClick={() => alert(index)}>
+            <li
+              key={index}
+              onClick={() => revertUser(index, user.name, user.Category)}
+            >
               {user.name} ({user.Category})
             </li>
           ))}
